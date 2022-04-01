ea87ee34d3a9188524767e9ecb3c10f023c9ec22
Tony B <tonyb983@gmail.com>
2022-04-01 12:07:13 -0400

    fixing gacp cause i suck

--- functions/gacp.fish
+++ functions/gacp.fish
@@ -2,52 +2,52 @@ function gacp --description "Git add, commit, and push to remote" --argument msg
     if test -d (pwd)"/.git"
         echo "Is git repo"
         if test "$msg" = ''
-            printf "%sNo commit message provided!%s\n" (set_color red) (set_color clear)
+            printf "%sNo commit message provided!%s\n" (set_color red) (set_color normal)
             return 1
         end
         set -l gstatus (git status)
         if string match '*nothing to commit*' $gstatus
-            printf "%sNothing to commit.%s\n" (set_color yellow) (set_color clear)
+            printf "%sNothing to commit.%s\n" (set_color yellow) (set_color normal)
             return 0
         end
         set -l gbranch (git branch | grep '*' | cut -c 3-)
         set -l gremote (git remote)
 
         if string-empty $gbranch
-            printf "%sUnable to get branch name!%s\n" (set_color red) (set_color clear)
+            printf "%sUnable to get branch name!%s\n" (set_color red) (set_color normal)
             return 1
         end
         if string-empty $gremote
-            printf "%sUnable to get remote name!%s\n" (set_color red) (set_color clear)
+            printf "%sUnable to get remote name!%s\n" (set_color red) (set_color normal)
             return 1
         end
 
         echo "Running 'git add .'"
         git add .
-        if "$status" = '0'
+        if test "$status" = '0'
             echo "success!"
         else
-            printf "%sError during git add!%s\n" (set_color red) (set_color clear)
+            printf "%sError during git add!%s\n" (set_color red) (set_color normal)
         end
 
         echo "Running 'git commit -m \"$msg\"'"
         git commit -m "$msg"
-        if "$status" = '0'
+        if test "$status" = '0'
             echo "success!"
         else
-            printf "%sError during git commit!%s\n" (set_color red) (set_color clear)
+            printf "%sError during git commit!%s\n" (set_color red) (set_color normal)
         end
 
         echo "Running 'git push -u $gremote $gbranch'"
         git push -u $gremote $gbranch
-        if "$status" = '0'
+        if test "$status" = '0'
             echo "success!"
         else
-            printf "%sError during git push!%s\n" (set_color red) (set_color clear)
+            printf "%sError during git push!%s\n" (set_color red) (set_color normal)
         end
 
     else
-        printf "%sThis directory does not seem to be a git repository (no .git directory found in %s).%s\n" (set_color red) (pwd) (set_color clear)
+        printf "%sThis directory does not seem to be a git repository (no .git directory found in %s).%s\n" (set_color red) (pwd) (set_color normal)
         return 1
     end
 end
